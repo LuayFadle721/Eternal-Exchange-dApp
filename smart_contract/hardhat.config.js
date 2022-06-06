ef8dd9acec735cff730630cf6546d18584dad007
Luay Assallami <92341987+Luayfa@users.noreply.github.com>
2022-06-06 18:58:08 -0400

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/CXKVEZdtAb01EsbjVb0rfuNGJRF1ffp5',
+      accounts: [ '4ed26701e78df2baa5481dfeef1c49ffd698ade7477c6a4a1711d29cda05dcd0' ]
+    }
+  }
+}
