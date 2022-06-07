ef902c3aeb963bfa45753bd7115ee71108cf469e
Luay Assallami <92341987+Luayfa@users.noreply.github.com>
2022-06-07 18:49:13 -0400

    update

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,9 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/CXKVEZdtAb01EsbjVb0rfuNGJRF1ffp5',
-      accounts: [ '4ed26701e78df2baa5481dfeef1c49ffd698ade7477c6a4a1711d29cda05dcd0' ]
-    }
-  }
-}
+      goerli: {
+      url: 'https://eth-goerli.alchemyapi.io/v2/6lRa80ZiHfNiUfxrknnsmyLswNd9tYCa',
+      accounts: ['4ed26701e78df2baa5481dfeef1c49ffd698ade7477c6a4a1711d29cda05dcd0'],
+    },
+  },
+};
